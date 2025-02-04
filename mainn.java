166a1ffb7a13d59f7f99bfae63e341367f27228e
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-04 09:41:16 -0500

    Update mainn.java

--- mainn.java
+++ mainn.java
@@ -2,10 +2,51 @@
 class Main{
   public static void main(String[] args){
 
-    
     System.out.println("A");
     Systen.out.printn("  or ");
     System.out.printf("");
-  //   
+    
+    int num; //whole numbers
+    boolean trueFalse; //true or false
+    String words; // strings of text
+    char c; //single character
+    double fractions; //doubles - fractional nums
+    float decimalll; //funny things that keep decimals to a certain point
+    
+    //variable naming conventions: START WITH A LOWERCASE LETTERS, LETTERS, PLS
+    //CLASSES NAMING START WITH A UPPERCASE
+    //  if your variable has more than one word, it starts the second word with uppercase
+    //camelCase <- like tis
+    // variables should be obvious 
+    // if its a radius NAME IT RADIUSSS
+    
+    num = 4;
+    //num now stores the value 4
+    //NOT NUM EQUAL FOUR
+    
+    words = "hellow worllddd";
+    // double qoutes for strings
+
+    trueFalse = true;
+    //booleans in java is all lowercase
+    
+    fractions = 3.5
+    c = '!';
+    //single qoute for char, stores one 
+
+    // strings - escape characters
+    // "\" alters meaning of the following code
+    System.out.println("a\na");
+    // \n new line
+    // \t tab
+    // \" changes double qoutes into a character instead of the line ending code.
+    // \\ changes escape character into a normal backslash
+    
+    // variable - using variable at other points in the program
+    // when variables are used anywhere except the left side of the equal side, it is a variable call
+
+    System.out.println(word);
+    
+    
   }
 }
