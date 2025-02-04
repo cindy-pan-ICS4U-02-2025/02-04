7f6b189d88b04534118cd1f2f61ebf4d73164409
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-04 09:24:06 -0500

    Update mainn.java

--- mainn.java
+++ mainn.java
@@ -1,2 +1,8 @@
 # YAAAAAAAYYYYYYYYYYYYYYYY
-  
+class Main{
+  public static void main(String[] args){
+    System.out.println("A");
+    Systen.out.printn("  or ");
+    System.out.printf("");
+  }
+}
